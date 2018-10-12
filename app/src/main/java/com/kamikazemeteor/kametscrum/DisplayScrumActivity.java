6c4bd56c010312469b3a176e93bf44a0a9ca4fc6
Ray Cooley <r0cool01@gmail.com>
2018-10-11 22:15:02 -0400

    required framework update
    updated target SDK framework to 26, remove motion-activated revealing.

--- app/src/main/java/com/kamikazemeteor/kametscrum/DisplayScrumActivity.java
+++ app/src/main/java/com/kamikazemeteor/kametscrum/DisplayScrumActivity.java
@@ -2,6 +2,10 @@ package com.kamikazemeteor.kametscrum;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,9 +13,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class DisplayScrumActivity extends AppCompatActivity {
+public class DisplayScrumActivity extends AppCompatActivity implements SensorEventListener {
     private static Boolean numberRevealed;
     private static int imgSrc;
+    //private static SensorManager sensorMgr;
+    //private static Sensor accelSensor;
+    private static float x, y, z;
+    private static final float ERROR = 7.0f;
+    private static boolean init;
+    private static boolean isFlipped;
 
     public static class TileBackFragment extends Fragment {
         @Override
@@ -23,16 +33,17 @@ public class DisplayScrumActivity extends AppCompatActivity {
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                getFragmentManager()
-                        .beginTransaction()
-                        .setCustomAnimations(
-                                R.animator.tile_flip_right_in,
-                                R.animator.tile_flip_right_out,
-                                R.animator.tile_flip_left_in,
-                                R.animator.tile_flip_left_out)
-                        .replace(R.id.scrumDisplay, new TileFrontFragment())
-                        .addToBackStack(null)
-                        .commit();
+                    getFragmentManager()
+                            .beginTransaction()
+                            .setCustomAnimations(
+                                    R.animator.tile_flip_right_in,
+                                    R.animator.tile_flip_right_out,
+                                    R.animator.tile_flip_left_in,
+                                    R.animator.tile_flip_left_out)
+                            .replace(R.id.scrumDisplay, new TileFrontFragment())
+                            .addToBackStack(null)
+                            .commit();
+                    isFlipped = false;
                 }
             });
 
@@ -41,10 +52,12 @@ public class DisplayScrumActivity extends AppCompatActivity {
     }
 
     public static class TileFrontFragment extends Fragment {
+        private ImageView imageView;
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            ImageView imageView = (ImageView)inflater.inflate(R.layout.tile_front,
+            imageView = (ImageView)inflater.inflate(R.layout.tile_front,
                     container, false);
             imageView.setImageResource(imgSrc);
             imageView.setOnClickListener(new View.OnClickListener() {
@@ -62,6 +75,11 @@ public class DisplayScrumActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_scrum);
 
+        //sensorMgr = (SensorManager)getSystemService(SENSOR_SERVICE);
+        //accelSensor = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //sensorMgr.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        init = false;
+
         TileBackFragment backTile = new TileBackFragment();
         getFragmentManager()
                 .beginTransaction()
@@ -71,5 +89,67 @@ public class DisplayScrumActivity extends AppCompatActivity {
         numberRevealed = false;
         Intent intent = getIntent();
         imgSrc = intent.getIntExtra(MainActivity.IMAGE_SOURCE, 0);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float a, b, c;
+        a = event.values[0];
+        b = event.values[1];
+        c = event.values[2];
+
+        if (!init) {
+            x = a;
+            y = b;
+            z = c;
+            init = true;
+        }
+        else {
+            float diffX = Math.abs(x - a);
+            float diffY = Math.abs(y - b);
+            float diffZ = Math.abs(z - c);
+
+            if (diffX < ERROR) { diffX = 0.0f; }
+            if (diffY < ERROR) { diffY = 0.0f; }
+            if (diffZ < ERROR) { diffZ = 0.0f; }
+
+            x = a;
+            y = b;
+            z = c;
+
+            if (diffX > diffY && !isFlipped) {
+                getFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(
+                                R.animator.tile_flip_right_in,
+                                R.animator.tile_flip_right_out,
+                                R.animator.tile_flip_left_in,
+                                R.animator.tile_flip_left_out)
+                        .replace(R.id.scrumDisplay, new TileFrontFragment())
+                        .addToBackStack(null)
+                        .commit();
+                isFlipped = true;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        isFlipped = false;
+    }
+
+    protected void onResume() {
+        super.onResume();
+        //sensorMgr.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    protected  void onPause() {
+        super.onPause();
+        //sensorMgr.unregisterListener(this);
     }
 }
